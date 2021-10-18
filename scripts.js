c6c5efe3aa1eb76f6f4967d83cd7405f7d079903
Tahjay Thompson <thompsontahjay34@gmail.com>
2021-10-18 18:51:26 -0500

    Added js to file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+// window.onload(){
+    const input = document.getElementsById("email");
+    input.preventDefault();
+    const message = documentByClassName("message");
+
+    const btn = documentByClassName("btn");
+    btn.addEventListener("click",(e)=>{
+        e.preventDefault();
+
+
+        if (input.value==""){
+            message.innerHTML="Please enter a Valid email address"
+        }
+        else if(input.value!=""){
+            message.innerHTML=`Thank you! Your email address ${input.value}  has been added to our mailing list`
+
+        }
+    })
+
+    
+
+// }
